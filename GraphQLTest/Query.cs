41ba0551101683ea27029d0b2af3000e4d376949
Stefan Bertens <stefan.bertens@vecozo.nl>
2021-11-19 10:10:30 +0100

    initial

---
+++ GraphQLTest/Query.cs
@@ -0,0 +1,20 @@
+﻿using HotChocolate;
+using Microsoft.AspNetCore.Http;
+
+namespace GraphQLTest
+{
+    public class Query
+    {
+        public Something getSomething([Service] IHttpContextAccessor contextAccessor)
+        {
+            Something result = null;
+
+            if (result == null)
+            {
+                contextAccessor.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GraphQLTest/Startup.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GraphQLTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>();
+
+            services
+                .AddRouting()
+                .AddHttpContextAccessor();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+        }
+    }
+}
